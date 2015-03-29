c7282935a66785977b3151fa6be30fc336575867
Captain kurO <me@captainkuro.com>
2015-03-29 18:33:42 +0700

    move resolving logic to enum

--- src/main/java/SortingHat.java
+++ src/main/java/SortingHat.java
@@ -1,14 +1,35 @@
 public class SortingHat {
+    private enum ItemType {
+        AGED_BRIE("AgedBrie", "Aged Brie"),
+        BACKSTAGE_PASSES("BackstagePasses", "Backstage passes to a TAFKAL80ETC concert"),
+        SULFURAS("Sulfuras", "Sulfuras, Hand of Ragnaros"),
+        CONJURED("Conjured", "Conjured Mana Cake"),
+        NORMAL("Normal", "");
+        
+        private final String className;
+        private final String itemName;
+        
+        private ItemType(String className, String itemName) {
+            this.className = className;
+            this.itemName = itemName;
+        }
+        
+        private static ItemType matches(Item item) {
+            for (ItemType t : ItemType.values()) {
+                if (t.itemName.equals(item.getName())) {
+                    return t;
+                }
+            }
+            return NORMAL;
+        }
+    }
     public static ItemUpdater assign(Item item) {
-        if (AgedBrie.NAME.equals(item.getName())) {
-            return new AgedBrie(item);
-        } else if (BackstagePasses.NAME.equals(item.getName())) {
-            return new BackstagePasses(item);
-        } else if (Sulfuras.NAME.equals(item.getName())) {
-            return new Sulfuras(item);
-        } else if (Conjured.NAME.equals(item.getName())) {
-            return new Conjured(item);
-        } else {
+        ItemType type = ItemType.matches(item);
+        try {
+            return (ItemUpdater)Class.forName(type.className)
+                    .getDeclaredConstructor(Item.class)
+                    .newInstance(item);
+        } catch(Exception ex) {
             return new Normal(item);
         }
     }
